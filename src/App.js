609dea6f211c16bb04fd801e1005a0c3cd22ad00
Siddhant <bhandarysid@gmail.com>
2023-08-04 11:37:25 +0530

    ain

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ export default function App() {
       window.addEventListener('scroll',()=>
       {
         const scrolled = window.scrollY;
-        (scrolled<=570)?setcolor(0):<></>;
-        (scrolled>=570)?setcolor(1):<></>;
-        (scrolled>=1570)?setcolor(2):<></>;
-        (scrolled>=2670)?setcolor(3):<></>;
+        if(scrolled<=570){setcolor(0)}
+        if(scrolled>=570){setcolor(1)}
+        if(scrolled>=1570){setcolor(2)}
+        if(scrolled>=2670){setcolor(3)}
       })
     }
 
